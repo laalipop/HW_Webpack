8803407e93b4ac39952165705fb786e5c7fa2ce2
laalipop <preicpodhya@mail.ru>
2020-12-23 15:43:50 +0300

    Удалила задачу 2

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,3 @@
-const game = new Game();
-game.start();
-
-import Game, { loadGame, saveGame } from './game.js';
-
-import sum from './basic';
-
 console.log('worked');
 
 console.log(sum([1, 2]));
